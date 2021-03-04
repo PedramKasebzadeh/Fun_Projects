6fede8acc9d23ea8bf31ea5ed52b4491b64eff55
Pedram Kasebzadeh <Pedram.kasebzadeh@gmail.com>
2021-03-04 11:25:35 +0100

    Lab1

---
+++ TDDE18/Lab1/1_inNout.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int user_input;
+    double real; 
+    char character;
+    string word;
+
+//Ex1
+// ex1.1 
+    // std::cout<< "Enter one integer: "<<std::endl;
+    // std:: cin >> user_input; 
+    // std:: cout << "You entered the number: " << user_input << std::endl;
+// ex1.2
+    // std::cout << "Enter four integers: ";
+    // std::cin >> user_input;
+    // std::cout << "You entered the numbers: " << user_input << " ";
+    // std::cin >> user_input;
+    // std::cout << user_input << " ";
+    // std::cin >> user_input;
+    // std::cout << user_input << " ";
+    // std::cin >> user_input;
+    // std::cout << user_input << "\n"<<std::endl;
+// ex1.3
+    //  std::cout << "Enter one integer and one read number:";
+    //  cin >> user_input >> real; 
+    //  cout<< "The real is:\t" << setprecision(4) << setw(12) <<real << endl; 
+    //  cout << "The integer is: \t" << user_input << endl; 
+// ex1.4
+    // cout << "Enter one real and one integer number:";
+    // cin >> real >> user_input; 
+    // cout<< "The real is: " << setprecision(4)<<fixed << setfill('.') << setw(9)<<real << endl; 
+    // cout << "The integer is: "<< setfill('.') << setw(6) << user_input << endl; 
+// ex1.5
+    // cout << "Enter a character: " ; 
+    // cin >> character; 
+    // cout << "Youe entered: " << character<< endl;
+    // ex1.6
+    // cout << "Enter a word: " <<endl; 
+    // cin >> word; 
+    // cout << "The word " << word << "has " << word.size() <<" character(s)." << endl;
+}

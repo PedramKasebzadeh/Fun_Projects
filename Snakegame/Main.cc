b33c628815809cce9751ef06386effe978d3adbf
Pedram Kasebzadeh <Pedram.kasebzadeh@gmail.com>
2021-03-26 15:43:49 +0100

    SnakeGame Final!

--- Snakegame/Main.cc
+++ Snakegame/Main.cc
@@ -1,27 +1,19 @@
 #include <cstdlib>
-#include <ncurses.h> // input output
+#include <ncurses.h>
 #include <iostream>
 
 
-#define KEY_UP 0403 // 
-#define KEY_DOWN 0402 //
-#define KEY_LEFT 0404 // 
-#define KEY_RIGHT 0405 // 
-
-using namespace std;
-
 bool GameOver;
 
-// Initialize the size of the game board
+// Initialize the size of the game boardconst int width=20;
 const int width=20;
-const int height = 20; 
+const int height=20;
 // Coordinates for snake and fruit and a possible score 
-int x,y,fx,fy, score; 
-int tailX[100], tailY[100];
-int nTail;
-enum eDirection {STOP=0,LEFT,RIGHT,UP,DOWN}; // Possible directions snake can go
+int x,y,xd,yd,score;
+int xtail[100], ytail[100];
+int ntail;
+enum eDirection {STOP=0,LEFT,RIGHT,UP,DOWN};// Possible directions snake can go
 eDirection dir;
-
 
 void Setup()
 {
@@ -31,55 +23,53 @@ void Setup()
     cbreak();
     curs_set(0);
 
-    GameOver = false;
+    GameOver= false;
     dir= STOP ; // Start without any Directions
     // Start at the middle of the game board
     x= width/2;
     y= height/2; 
-    // Fruit at a random place in the game board 
-    fx=  rand() % width; // gives a random number in the width range(0 to width-1).
-    fy=  rand() % height;
-    score = 0;
+    // Diamonds at a random place in the game board 
+    xd = rand() % width;// gives a random number in the width range(0 to width-1).
+    yd = rand() % height;
+    score=0;
 }
 
-
-void Board() {
+void Board(){
 //screen clear for every instance to transform into animation.
- clear();
-    for(int i = 0; i < width+2; i++)
-        mvprintw(0,i,"+");
-    for(int i = 0; i < height+2; i++)
+clear();
+    for (int i = 0; i < width+2; i++)
+        mvprintw(0,i,"#");
+    for (int i = 0; i< height+2; i++)
     {
-        for(int j = 0; j < width+2; j++)
+        for (int j = 0; j < width+2; j++)
         {
-            if (i == 0 || i == 21)
-                mvprintw(i, j, "+");
-            else if (j == 0 || j == 21)
-                mvprintw(i, j, "+");
-            else if (i == y && j == x)
-                mvprintw(i, j, "O");
-            else if(i == fy && j == fx)
-                mvprintw(i, j, "Q");
-                  else
-            {
-              for(int k = 0; k < nTail; k++)
-              {
-                if (tailX[k] == j && tailY[k] == i)
-
-                {
-                  mvprintw(i, j, "o");
-                }
-              }
-            }      
-        }
+            if (i == 0 || i== width+1) // top and bottom
+                mvprintw(i,j,"#"); 
+            else if (j == 0 || j== height+1) // side borders 
+                mvprintw(i,j,"#");
+            else if (i == y && j== x) // Head of snake
+                 mvprintw(i,j,"Q");  
+            else if (i == yd && j== xd) // Diomand
+                     mvprintw(i,j,"*"); 
+                     else
+                {      
+                    for ( int k = 0; k < ntail; k++)
+                    {
+                        if(xtail[k]==j && ytail[k]==i)
+                        {
+                            mvprintw(i,j,"o");
+                        }  
+                    } 
+                }  
+          }
     }
-    mvprintw(23, 0, "Score %d", score);
+    mvprintw(height+5,0,"Score is: %d", score);
     refresh();
 }
 
-
-void Input(){
-    keypad(stdscr, true);
+void Input()
+{
+     keypad(stdscr, true);
   halfdelay(1);
   int c = getch();
   switch(c)
@@ -96,28 +86,29 @@ void Input(){
     case KEY_DOWN:
       dir = DOWN;
       break;
-    case 'Q':
+    case '113': // 113 represents q for quit 
       GameOver = true;
       break;
     }
 }
+
 void logic()
 {
- int prevX = tailX[0];
-  int prevY = tailY[0];
+    int prevX = xtail[0];
+  int prevY = ytail[0];
   int prev2X, prev2Y;
-  tailX[0] = x;
-  tailY[0] = y;
-  for (int i = 1; i < nTail; i++)
+  xtail[0] = x;
+  ytail[0] = y;
+  for (int i = 1; i < ntail; i++)
   {
-    prev2X = tailX[i];
-    prev2Y = tailY[i];
-    tailX[i] = prevX;
-    tailY[i] = prevY;
+    prev2X = xtail[i];
+    prev2Y = ytail[i];
+    xtail[i] = prevX;
+    ytail[i] = prevY;
     prevX = prev2X;
     prevY = prev2Y;
   }
-  switch(dir)
+    switch(dir)
   {
     case LEFT:
       x--;
@@ -134,21 +125,20 @@ void logic()
     default:
       break;
   }
-  if(x > width || x < 1 || y < 1 || y > height)
-    GameOver = true;
-  if(x == fx && y == fy)
-  {
-    score++;
-    fx = (rand() % width)+1;
-    fy = (rand() % height)+1;
-    nTail++;
-  }
-  for (int i = 0; i < nTail; i++)
-    if (tailX[i] == x && tailY[i] == y)
+    if(x > width || x < 1 || y < 1 || y > height)
+    GameOver = true; 
+    if(x == xd && y==yd) 
     {
-      GameOver = true;
+        score++ ;
+        xd = (rand() % width)+1;
+        yd = (rand() % height)+1;
+        ntail++;
     }
+    for (int i=0 ; i < ntail ; i++)
+        if(xtail[i]==x && ytail[i]==y)
+            GameOver=true;
 }
+
 int main(){
     Setup();
     Board();

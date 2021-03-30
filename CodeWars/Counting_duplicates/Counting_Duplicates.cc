f0682663727d9ac472df29fed372af8bbc83e1ca
Pedram Kasebzadeh <Pedram.kasebzadeh@gmail.com>
2021-03-30 13:52:18 +0200

    Counting_duplicates

---
+++ CodeWars/Counting_duplicates/Counting_Duplicates.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    std::string str = "aabbcde";
+    int i = 0;
+    for(char c : str){
+    cout << c <<endl;
+    if(c != str[i]){
+        
+    }
+
+    }
+    return 0;
+}
